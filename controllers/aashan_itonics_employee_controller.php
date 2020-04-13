e36ad7bdf0613ab71f6133ac38767c85c4964527
Aashan Ghimire <ashanghimire10@gmail.com>
2020-04-13 13:10:20 +0545

    create controller

---
+++ controllers/aashan_itonics_employee_controller.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Controller action for showing employee list
+ */
+function aashan_itonics_get_employee_list() {
+
+  return theme('aashan_itonics_employee_list', ['data' => []]);
+}
