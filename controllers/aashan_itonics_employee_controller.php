3f55264b9646cae0be7d8a9d1fed6555228d21b5
Aashan Ghimire <ashanghimire10@gmail.com>
2020-04-13 14:08:57 +0545

    provide data for template

--- controllers/aashan_itonics_employee_controller.php
+++ controllers/aashan_itonics_employee_controller.php
@@ -4,6 +4,6 @@
  * Controller action for showing employee list
  */
 function aashan_itonics_get_employee_list() {
-
-  return theme('aashan_itonics_employee_list', ['data' => []]);
+  $data = db_select(AASHAN_ITONICS_DB_TABLE)->fields(AASHAN_ITONICS_DB_TABLE)->execute()->fetchAllAssoc('id');
+  return theme('aashan_itonics_employee_list', ['data' => $data]);
 }
