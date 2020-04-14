bb3dc6fb8640047441fe8bc527b067440640490e
Aashan Ghimire <ashanghimire10@gmail.com>
2020-04-14 11:03:07 +0545

    remove additional details from list page template

--- templates/aashan_itonics_employee_list.tpl.php
+++ templates/aashan_itonics_employee_list.tpl.php
@@ -18,7 +18,6 @@ global $base_url;
     <th>Email</th>
     <th>Photo</th>
     <th>Projects</th>
-    <th>Additional Info</th>
     <th>Actions</th>
     </thead>
     <tbody>
@@ -26,16 +25,15 @@ global $base_url;
       <?php foreach ($data as $employee) : ?>
         <tr>
           <td><?= $employee->id ?></td>
-          <td><?= $employee->name ?></td>
+          <td><?= htmlentities($employee->name) ?></td>
           <td><?= $employee->gender == 1 ? 'Male' : ($employee->gender == 0 ? 'Female' : 'Others') ?></td>
-          <td><?= ucfirst($employee->address) ?></td>
-          <td><?= $employee->email ?></td>
+          <td><?= htmlentities($employee->address) ?></td>
+          <td><?= htmlentities($employee->email) ?></td>
           <td>
             <img src="<?= $base_url . '/sites/default/files/' . $employee->image ?>" alt="<?= $employee->name ?>"
                  height="100" width="100"/>
           </td>
-          <td><?= $employee->projects ?></td>
-          <td><?= $employee->remarks ?></td>
+          <td><?= htmlentities($employee->projects) ?></td>
           <td>
             <a href="<?= $base_url . '/employee/' . $employee->id . '/edit' ?>">Edit</a>
             <a href="<?= $base_url . '/employee/' . $employee->id . '/' ?>">View</a>
@@ -59,18 +57,4 @@ global $base_url;
     <?php endif; ?>
     </tbody>
   </table>
-  <?php if (count($data)) : ?>
-    <script type="text/javascript">
-      let deleteButtons = document.getElementsByClassName('delete-employee');
-      for (let i = 0; i < deleteButtons.length; i++) {
-        deleteButtons[i].addEventListener('click', function (event) {
-          let url = event.target.getAttribute('data-href');
-          let confirmation = confirm('Are you sure you want to delete this employee?');
-          if (confirmation) {
-            window.location.href = url;
-          }
-        });
-      }
-    </script>
-  <?php endif; ?>
 </div>
