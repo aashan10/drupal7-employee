de7bb1b8d8255a5f8595d99e274dbd0f39e42f8e
Aashan Ghimire <ashanghimire10@gmail.com>
2020-04-14 10:47:46 +0545

    update employee list template

--- templates/aashan_itonics_employee_list.tpl.php
+++ templates/aashan_itonics_employee_list.tpl.php
@@ -1,44 +1,76 @@
-<?php /** @var $data array */ global $base_url; ?>
+<?php
+
+/** @var $data array */
+
+global $base_url;
+
+?>
 <div class="aashan_itonics_employee">
   <div class="employee-header">
     <a href="<?= $base_url . '/employee/create' ?>" class="button">+ Add Employee</a>
   </div>
-  <?php if (is_array($data) && count($data) > 0) : ?>
-    <table>
-      <thead>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Gender</th>
-        <th>Address</th>
-        <th>Email</th>
-        <th>Photo</th>
-        <th>Projects</th>
-        <th>Additional Info</th>
-        <th>Actions</th>
-      </thead>
-      <tbody>
+  <table>
+    <thead>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Gender</th>
+    <th>Address</th>
+    <th>Email</th>
+    <th>Photo</th>
+    <th>Projects</th>
+    <th>Additional Info</th>
+    <th>Actions</th>
+    </thead>
+    <tbody>
+    <?php if (is_array($data) && count($data) > 0) : ?>
       <?php foreach ($data as $employee) : ?>
-      <tr>
-        <td><?= $employee->id ?></td>
-        <td><?= $employee->name ?></td>
-        <td><?= ucfirst($employee->gender) ?></td>
-        <td><?= ucfirst($employee->address) ?></td>
-        <td><?= $employee->email ?></td>
-        <td>
-          <img src="<?= $base_url . '/sites/default/employee/' . $employee->image?>" alt="<?= $employee->name ?>" height="100" width="100"/>
-        </td>
-        <td><?= $employee->projects ?></td>
-        <td><?= $employee->remarks ?></td>
-        <td>
-          <a href="<?= $base_url . '/employee/' . $employee->id . '/edit' ?>">Edit</a>
-          <a href="<?= $base_url . '/employee/' . $employee->id . '/' ?>">View</a>
-          <a href="<?= $base_url . '/employee/' . $employee->id . '/delete' ?>">Delete</a>
-        </td>
-      </tr>
+        <tr>
+          <td><?= $employee->id ?></td>
+          <td><?= $employee->name ?></td>
+          <td><?= $employee->gender == 1 ? 'Male' : ($employee->gender == 0 ? 'Female' : 'Others') ?></td>
+          <td><?= ucfirst($employee->address) ?></td>
+          <td><?= $employee->email ?></td>
+          <td>
+            <img src="<?= $base_url . '/sites/default/files/' . $employee->image ?>" alt="<?= $employee->name ?>"
+                 height="100" width="100"/>
+          </td>
+          <td><?= $employee->projects ?></td>
+          <td><?= $employee->remarks ?></td>
+          <td>
+            <a href="<?= $base_url . '/employee/' . $employee->id . '/edit' ?>">Edit</a>
+            <a href="<?= $base_url . '/employee/' . $employee->id . '/' ?>">View</a>
+            <a class="delete-employee"
+               data-href="<?= $base_url . '/employee/' . $employee->id . '/delete' ?>">Delete</a>
+          </td>
+        </tr>
       <?php endforeach; ?>
-      </tbody>
-    </table>
-  <?php else: ?>
-    <p>There are no records!</p>
+    <?php else: ?>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>There are no records!</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    <?php endif; ?>
+    </tbody>
+  </table>
+  <?php if (count($data)) : ?>
+    <script type="text/javascript">
+      let deleteButtons = document.getElementsByClassName('delete-employee');
+      for (let i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener('click', function (event) {
+          let url = event.target.getAttribute('data-href');
+          let confirmation = confirm('Are you sure you want to delete this employee?');
+          if (confirmation) {
+            window.location.href = url;
+          }
+        });
+      }
+    </script>
   <?php endif; ?>
 </div>
